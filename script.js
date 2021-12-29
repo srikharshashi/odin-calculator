e907e63630cb5ba9c5889b5f05f1a85e24ccb1b6
Srikhar Shashi D <dasojusrikhar@gmail.com>
2021-12-29 20:15:11 +0530

    incomplete

---
+++ script.js
@@ -0,0 +1,12 @@
+const calctextdiv = document.querySelector(".cont1");
+const calcobj={
+    operand1:0,
+    operator:0,
+    operand2:0,
+    result: undefined,
+};
+calctextdiv.textContent=
+const buttons = document.querySelectorAll(".buttons");
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {});
+});
